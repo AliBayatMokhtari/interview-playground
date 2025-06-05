6007ee8de6dce5096d4122dd4eeae3345a7c76e8
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2025-06-05 10:09:10 +0330

    feat: implement custom hook for local storage management and integrate pane size persistence in Playground component

--- src/components/playground/Playground.tsx
+++ src/components/playground/Playground.tsx
@@ -3,14 +3,28 @@ import { Allotment } from "allotment";
 
 // @ts-expect-error
 import "allotment/dist/style.css";
+import useStorageValue, { StorageKey } from "src/hooks/useStorageValue";
+
+type PaneSize = string | number;
 
 function Playground() {
+  const [[left, right], setPanes] = useStorageValue<[PaneSize, PaneSize]>(
+    StorageKey.playgroundPanes,
+    ["50%", "50%"]
+  );
+
+  const handleSizeChange = (sizes: number[]) => {
+    const [left, right] = sizes;
+
+    setPanes([left, right]);
+  };
+
   return (
-    <Allotment onChange={console.log}>
-      <Allotment.Pane minSize={400}>
+    <Allotment onDragEnd={handleSizeChange}>
+      <Allotment.Pane minSize={400} preferredSize={left}>
         <Box p={2}>Pane 1</Box>
       </Allotment.Pane>
-      <Allotment.Pane minSize={400}>
+      <Allotment.Pane minSize={400} preferredSize={right}>
         <Box p={2}>Pane 2</Box>
       </Allotment.Pane>
     </Allotment>
