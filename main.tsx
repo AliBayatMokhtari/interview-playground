a2b02a1fe22c30c83648f415cc0f119c9cc3f90a
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2025-07-11 20:26:55 +0330

    chore: rename

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./src/app";
+import App from "./src/Application";
 import "./src/utils/boot";
 
 const app = document.getElementById("app")!;
